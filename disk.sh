53c6235571f6df51ec3eff3023d6ef2545eecb5b
Ricardo Catalinas Jiménez <r@untroubled.be>
2018-02-20 12:37:10 +0000

    .

--- disk.sh
+++ disk.sh
@@ -7,11 +7,15 @@ die() {
 	exit 1
 }
 
+announce() {
+	echo "==> $*"
+}
+
 find_disk_dev() {
 	local -r model=$1
 	local dev=$(lsblk -S -o kname,model | awk "/$model/"'{print $1}')
-	mount | grep /dev/$dev >/dev/null
-	if [[ $? == 0 ]]
+
+	if mount | grep /dev/$dev >/dev/null
 	then
 		die "disk is mounted"
 	fi
@@ -21,10 +25,15 @@ find_disk_dev() {
 prepare_disk() {
 	local -r dev=$1
 
-	parted -s
+	parted -s $dev -a optimal \
+		mklabel gpt \
+		mkpart primary 0% 513MiB \
+		mkpart primary 513MiB 100% \
+		set 1 boot on
 }
 
 DISK_MODEL='Samsung SSD 850'
 DISK_DEV=/dev/$(find_disk_dev "$DISK_MODEL")
 
-echo DISK_DEV=$DISK_DEV
+announce Preparing $DISK_DEV
+prepare_disk $DISK_DEV
