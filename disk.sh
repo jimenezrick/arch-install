09114dfe0e877c2e8a2970809877e3b01648f995
Ricardo Catalinas Jiménez <r@untroubled.be>
2018-02-21 09:33:01 +0000

    .

--- disk.sh
+++ disk.sh
@@ -12,7 +12,7 @@ announce() {
 }
 
 find_disk_dev() {
-	local -r model=$1
+	local model=$1
 	local dev=$(lsblk -S -o kname,model | awk "/$model/"'{print $1}')
 
 	if mount | grep /dev/$dev >/dev/null
@@ -23,17 +23,28 @@ find_disk_dev() {
 }
 
 prepare_disk() {
-	local -r dev=$1
+	local dev=$1
 
+	announce Preparing $dev
 	parted -s $dev -a optimal \
 		mklabel gpt \
 		mkpart primary 0% 513MiB \
 		mkpart primary 513MiB 100% \
 		set 1 boot on
+
+	local dev_esp=${dev}1
+	local dev_rootfs=${dev}2
+
+	announce Formatting ESP $dev_esp
+	mkfs.fat -F32 $dev_esp
+
+	announce Formatting encrypted rootfs $dev_rootfs
+	cryptsetup -y -v luksFormat --type luks2 $dev_rootfs
+	cryptsetup open $dev_rootfs cryptroot
+	mkfs.btrfs -f /dev/mapper/cryptroot
 }
 
 DISK_MODEL='Samsung SSD 850'
 DISK_DEV=/dev/$(find_disk_dev "$DISK_MODEL")
 
-announce Preparing $DISK_DEV
 prepare_disk $DISK_DEV
