2473a76c86b72b1f62381c0f3c1971e76e654320
Ricardo Catalinas Jiménez <r@untroubled.be>
2018-03-10 22:08:16 +0000

    Add a single script to restore my stuff

--- extra/restore_my_stuff.sh
+++ extra/restore_my_stuff.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
-etc=$1
+home=$1
+etc=$home/archive/backup/etc
 
 mv -v /mnt/etc /mnt/etc2
 rsync --archive --delete --info=flist2,progress2 $etc/ /mnt/etc
 cp -v /mnt/etc2/fstab /mnt/etc
+
+rsync --archive --delete --info=flist2,progress2 $home/ /mnt/home
--- extra/rsync_home.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-home=$1
-
-rsync --archive --delete --info=flist2,progress2 $home/ /mnt/home
