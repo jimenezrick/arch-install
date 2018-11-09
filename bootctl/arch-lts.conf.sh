7b74f5333868fd1860c3f2c543a60df949efc698
Ricardo Catalinas Jiménez <r@untroubled.be>
2018-11-09 15:45:55 +0000

    initrd: Add /amd-ucode.img

--- bootctl/arch-lts.conf.sh
+++ bootctl/arch-lts.conf.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cat <<EOF
 title   Arch Linux LTS
 linux   /vmlinuz-linux-lts
+initrd  /amd-ucode.img
 initrd  /initramfs-linux-lts.img
 options root=/dev/mapper/cryptroot rootflags=subvol=@ luks.uuid=${1} luks.name=${1}=cryptroot rw iommu=soft
 EOF
