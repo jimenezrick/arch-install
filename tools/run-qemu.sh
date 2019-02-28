765f7c5e94a116da0248315506086f38cc4f2fd4
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-02-28 19:12:13 +0000

    tools: Run qemu always with -nographic

--- tools/run-qemu.sh
+++ tools/run-qemu.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 DIR=$(cd $(dirname $0); pwd)
 
-mkosi qemu -virtfs local,path=$DIR/..,security_model=passthrough,mount_tag=arch-rebuild "$@"
+mkosi qemu -virtfs local,path=$DIR/..,security_model=passthrough,mount_tag=arch-rebuild -nographic "$@"
