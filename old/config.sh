fbbaa94f072c244042e059cfdb6efc15c5a53adf
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-10-27 22:01:58 +0000

    old: remove replaced scripts

--- old/config.sh
+++
@@ -1,9 +0,0 @@
-export DISK_MODEL='Samsung SSD 850'
-export ZONEINFO='Europe/London'
-export LOCALE='en_US'
-export KEYMAP='us'
-export HOSTNAME='viper.local'
-
-export INSTALL_PKGS=($(cat $CWD/pkgs/explicit-pkgs.pacman))
-export INSTALL_GROUPS=($(cat $CWD/pkgs/groups-pkgs.pacman))
-export INSTALL_AUR=($(cat $CWD/pkgs/aur-pkgs.pacman))
