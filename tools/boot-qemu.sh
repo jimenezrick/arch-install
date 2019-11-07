9ad85964ff54c49ea731bbfa8cd80eb5b9702374
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-11-07 09:45:41 +0000

    boot-qemu.sh: Allow passing extra args

--- tools/boot-qemu.sh
+++ tools/boot-qemu.sh
@@ -16,28 +16,33 @@ run_qemu() {
 		# -nographic (boot kernel with console=ttyS0)
 }
 
-case $1 in
+cmd=$1
+shift
+case $cmd in
 	iso)
 		run_qemu \
 			-device virtio-scsi-pci,id=scsi0 \
 			-drive file=$DISK,if=none,format=raw,discard=unmap,aio=native,cache=none,id=hd0 \
 			-device scsi-hd,drive=hd0,bus=scsi0.0 \
 			-cdrom $DIR/$ISO \
-			-boot order=d
+			-boot order=d \
+			"$@"
 		;;
 	disk)
 		run_qemu \
 			-device virtio-scsi-pci,id=scsi0 \
 			-drive file=$DISK,if=none,format=raw,discard=unmap,aio=native,cache=none,id=hd0 \
 			-device scsi-hd,drive=hd0,bus=scsi0.0 \
-			-boot order=c
+			-boot order=c \
+			"$@"
 		;;
 	disk-blk)
 		run_qemu \
 			-drive file=$DISK,format=raw,cache=none,if=virtio \
-			-boot order=c
+			-boot order=c \
+			"$@"
 		;;
 	*)
-		echo "Usage: $0 iso|disk|disk-blk"
+		echo "Usage: $0 iso|disk|disk-blk [<extra_args>]"
 		;;
 esac
