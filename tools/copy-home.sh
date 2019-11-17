ab42af09249f68403ff6fdb9d6ba32d2d6e266ba
Ricardo Catalinas Jiménez <r@untroubled.be>
2019-11-17 12:09:33 +0000

    Don't use sudo directly on the scripts

--- tools/copy-home.sh
+++ tools/copy-home.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 DIR=$(cd $(dirname $0); pwd)
 
-sudo rsync --archive --delete --info=flist2,progress2 $(realpath $1) $2
+rsync --archive --delete --info=flist2,progress2 $(realpath $1) $2
