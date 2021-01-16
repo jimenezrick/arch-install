8dd15ca6003dac56edbf43b59a02494ace1fd3b2
Ricardo Catalinas Jiménez <r@untroubled.be>
2021-01-16 23:12:33 +0000

    qemu: Update 9p msize to 128 MiB to improve performance

--- tools/bootstrap-qemu.sh
+++ tools/bootstrap-qemu.sh
@@ -5,7 +5,7 @@ set -e
 pacman --noconfirm -Sy git
 echo Bootstrapping arch-rebuild from local repo...
 mkdir -p /mnt/arch-rebuild
-mount -t 9p -o trans=virtio,version=9p2000.L,rw arch-rebuild /mnt/arch-rebuild
+mount -t 9p -o trans=virtio,version=9p2000.L,msize=$((128 * 1024 * 1024)),rw arch-rebuild /mnt/arch-rebuild
 /mnt/arch-rebuild/arch-rebuild build-arch               \
 	-c /mnt/arch-rebuild/config/system.dhall        \
 	-e /mnt/arch-rebuild/restore/etc.bundle         \
